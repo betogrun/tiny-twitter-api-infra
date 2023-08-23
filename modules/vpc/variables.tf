4fa16b7e2673e576d9fb9790563d0f65ce6e8a0c
Alberto Rocha <betogrun@gmail.com>
2023-08-23 18:31:13 -0300

    Update vpc module

--- modules/vpc/variables.tf
+++ modules/vpc/variables.tf
@@ -3,7 +3,13 @@ variable "vpc_cidr_block" {
   default     = "10.0.0.0/16"
 }
 
-variable "subnet_cidr_block" {
-  description = "The CIDR block for the subnet"
-  default     = "10.0.0.0/24"
+variable "public_subnet_cidr_block" {
+  description = "The CIDR block for the public subnet"
+  default     = "10.0.1.0/24"
 }
+
+variable "private_subnet_cidr_block" {
+  description = "The CIDR block for the private subnet"
+  default     = "10.0.2.0/24"
+}
+
