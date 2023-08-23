811de16bfef74c9e7c118e2c030063da248781cb
Alberto Rocha <betogrun@gmail.com>
2023-08-23 18:32:23 -0300

    Update main and outputs files

--- main.tf
+++ main.tf
@@ -3,3 +3,9 @@ provider "aws" {}
 module "vpc" {
   source = "./modules/vpc"
 }
+
+module "ec2" {
+  source = "./modules/ec2"
+  subnet_id = module.vpc.public_subnet_id
+  vpc_id = module.vpc.vpc_id
+}
